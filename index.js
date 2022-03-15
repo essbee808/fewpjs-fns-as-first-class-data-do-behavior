ffaa05f0a9b1c8a5abb25b60fc51e5366969526b
Sheena Bonilla <sheena.myb@gmail.com>
2022-03-15 10:35:39 -0700

    Done.

--- index.js
+++ index.js
@@ -11,3 +11,20 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+function greet(timeString) {
+  const hour = parseInt(timeString, 10)
+  if (hour < 12) {
+    return `Good Morning`;
+  }
+  else if (hour > 17) {
+    return `Good Evening`;
+  }
+  else
+    return `Good Afternoon`;
+}
+  // if the time is earlier than 12 pm, return "Good Morning"
+  // if later than 5 pm, return "Good Evening"
+
+function displayMessage(msg) {
+  document.getElementById('greeting').textContent = msg;
+}
